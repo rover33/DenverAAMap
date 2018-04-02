642ebfca6ceb78df7f408fa503b7b1605f23a0ad
anhonestmouse <anhonestmouse@gmail.com>
2018-04-02 15:23:32 -0600

    data is saved as an array of objects

--- backend/controllers/mainController.js
+++ backend/controllers/mainController.js
@@ -1,19 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-
-
-
-
-//   'http://www.daccaa.org/query.asp',{
-//     options:{
-//         headers:{'Content-Type':'application/x-www-form-urlencoded'},
-//         form:{
-//             'cboDay':'0', 'cboStartTime':'All','cboEndTime':'All','cboMeetingType':'All','cboMeetingFormat':'All','cboSpecialMeeting':'All','cboDistrict':'All','cmdFindMeetings':'Find+Meetings'}
-//         }
-// {body:{'cboDay':'0', 'cboStartTime':'All','cboEndTime':'All','cboMeetingType':'All','cboMeetingFormat':'All','cboSpecialMeeting':'All','cboDistrict':'All','cmdFindMeetings':'Find Meetings'}
 let getMeetings = (req, res)=>{
-    console.log('Are we Crazy?');
     var settings = {
         url: "http://www.daccaa.org/query.asp",
         form: {
@@ -27,22 +15,26 @@ let getMeetings = (req, res)=>{
             "cmdFindMeetings": "Find Meetings"
         }
     }
-    console.log('It should get here though?');
     request.post(settings,(err,response,body)=>{
             if(err){
                 console.log('Error grabbing Meeting Data:',err);
                 res.send('HI!');
             }else{
-                console.log('WTF IS HAPPENING?!!')
-                console.log(' ');
-                console.log(' ');
-                console.log(' ');
-                console.log(' ');
-                console.log(' ');
-                console.log(' ');
-                console.log(' ');
-                console.log(' ');
-                console.log('SUCCESS!?:',response);
+                let $ = cheerio.load(body);
+                let stringArr=[];
+                $('font').each((index,element)=>{
+                    if(element.children && element.children[0] && element.children[0].data){
+                        stringArr.push(element.children[0].data.trim());
+                    }
+                });
+                let meetingArray = [];
+                for(let i = 0; i<stringArr.length; ++i){
+                    if(["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"].indexOf(stringArr[i])>=0){
+                        meetingArray.push({day:stringArr[i],time:stringArr[i+1],groupName:stringArr[i+2],address:stringArr[i+3],city:stringArr[i+4],codes:stringArr[i+6]});
+                        i=i+6;
+                    }
+                }
+                console.log(...meetingArray);
                 res.send(body);
             }
         });
