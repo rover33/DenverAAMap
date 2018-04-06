4dc4e3b07d5c393a44a2cc42d52d67f0c48decf9
rover33 <remy.j.pearlstone@gmail.com>
2018-04-06 15:49:20 -0600

    awkhw

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const mainController = require('../controllers/mainController');
 
-router.route('/meetings?*')
+router.route('/meeting123?*')
     .get(mainController.getMeetings);
 
 router.route('/meetings')
